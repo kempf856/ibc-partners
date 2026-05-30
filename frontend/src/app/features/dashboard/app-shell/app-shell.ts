799ab798af669529a922d6da027fb51d316c284f
Kertész Zoltán <Zoltan.Kertesz@ext.4ig.hu>
2026-05-30 20:05:40 +0200

    mobile fixes

--- frontend/src/app/features/dashboard/app-shell/app-shell.ts
+++ frontend/src/app/features/dashboard/app-shell/app-shell.ts
@@ -1,8 +1,10 @@
 import {Component, inject} from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
-import {MatButton} from '@angular/material/button';
-import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {Auth} from '../../../core/auth/auth';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-app-shell',
@@ -10,7 +12,12 @@ import {Auth} from '../../../core/auth/auth';
     MatToolbar,
     MatButton,
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    MatIconButton,
+    MatSidenavContent,
+    MatSidenav,
+    MatSidenavContainer,
+    MatIcon
   ],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
